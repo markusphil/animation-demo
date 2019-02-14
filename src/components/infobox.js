e3cf08ec3906d0b5ac12d37b58cf0b0f58682b48
markusphil <m.pilipp1991@web.de>
2019-02-14 10:31:57 +0100

    small adjustments

--- src/components/infobox.js
+++ src/components/infobox.js
@@ -5,13 +5,13 @@ import {CloseIcon} from '../graphics/close';
 export const InfoBox = (props) => (
     <div className="info-box">
         <div className="info-head">
-            <h3>Move your cursor over the objects to trigger animations.</h3>
+            <h3>Move your cursor over the objects.</h3>
             <button className="close-btn" onClick={props.close}>
                 <CloseIcon width="20px"/>
             </button> 
         </div>
         <div className="info-body">
-        <p><strong>Attention:</strong>This Demo is developed for desktop mouse-hover-events so it won't work properly on touchscreen devices, sorry!</p>
+        <p><strong>Attention:</strong> This demo is developed for desktop mouse-events and won't work with touchscreen input!</p>
         </div>
         
            
